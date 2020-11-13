4c158b0f77afaeff0ee42dd89a31dc4eac153239
relapse22 <mich3al.stark3@gmail.com>
2020-11-13 23:29:20 +0000

    second commit

--- learning_OOP/0.py
+++ learning_OOP/0.py
@@ -10,12 +10,17 @@ class PlayerCharacter:
 
 player1 = PlayerCharacter('Max', 10)
 player2 = PlayerCharacter('Abs', 18)
+player1.attack = 50
+player2.attack = 25
 
 print(player1.name)
 print(player1.age)
 print(player1.run())
+print(player1.attack)
 
 print(player2.name)
 print(player2.age)
+print(player2.attack)
+
 
 
