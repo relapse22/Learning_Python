52f26bd5625401c0fdaa850066a12e649364d54c
relapse22 <mich3al.stark3@gmail.com>
2020-11-14 00:13:01 +0000

    added more to fileIO

---
+++ learning_fileIO/read_file0.py
@@ -0,0 +1,14 @@
+my_file = open('test.txt')
+
+# (seek) Will go to the line in the Text File
+my_file.seek(0)
+print(my_file.read())
+my_file.seek(0)
+
+print(my_file.readline())
+print(my_file.readline())
+print(my_file.readline())
+my_file.seek(0)
+
+print() # empty print just to give a line space in the code
+print(my_file.readlines())
